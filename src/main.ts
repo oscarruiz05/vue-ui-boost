c0db3c72ad3a9dcd54ce76ae66a65de1845a65f0
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-07-26 00:08:05 -0500

    feat: :sparkles: add vue router

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './style.css'
 import App from './App.vue'
+import router from './routes'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
